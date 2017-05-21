0204f0e68f2363c2232236f4da4930563d89449d
Excelle Su <suwei779@126.com>
2017-05-21 14:20:31 +0800

    Browse through directories

--- web-ui/static/js/ftp-client.js
+++ web-ui/static/js/ftp-client.js
@@ -13,6 +13,17 @@ $(document).ready(function() {
         if (err) {
             alert(err.message);
         } else {
+            r.unshift({
+                name: '..',
+                mode: 'dr-xr-xr-x',
+                link_num: '',
+                owner: '',
+                group: '',
+                length: '',
+                month: '',
+                day: '',
+                time: ''
+            });
             initFileList(r);
         }
     })
@@ -33,6 +44,30 @@ function initFileList(files) {
             access: function(file) {
                 if (file.mode[0] == 'd') {
                     // Is directory
+                    var new_dir = '';
+                    var _this = this;
+                    if (file.name == '..') {
+                        if (this.working_dir == '/') {
+                            return;
+                        }
+                        var components = this.working_dir.split('/');
+                        components = components.splice(0, components.length - 2);
+                        new_dir = components.join('/') + '/';
+                    } else {
+                        new_dir = this.working_dir + file.name + '/';
+                    }
+                    this.working_dir = new_dir;
+                    current_dir = new_dir;
+                    getApi('/ftp/root' + new_dir, {}, function (err, r) {
+                        if (err) {
+                            alert(err.message);
+                        } else {
+                            _this.files.splice(1, _this.files.length - 1);
+                            for (var i in r) {
+                                _this.files.push(r[i]);
+                            }
+                        }
+                    });
                 } else {
                     var url = [
                         window.location.protocol, '//',
